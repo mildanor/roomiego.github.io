9c364ef82a3911520bd3aaae702c1563179d9772
Alessandro Capra <alessandro.cpr@gmail.com>
2017-03-06 23:39:43 +0200

    Added feedback when clicking pricing plans buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('#free_pm, #freemium_pm').on('click', function() {
+    $(this).next('.feedback-click').show().delay(2000).fadeOut(500);
+  });
+});
